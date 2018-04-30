3c618a27ccf3a59c7587e64ba46ab1a5ef7af973
lucas <lucas@lucas>
2018-04-29 22:55:30 -0300

    Update

---
+++ Ehealth/Test/Msg.py
@@ -0,0 +1,14 @@
+'''
+Created on 29 de abr de 2018
+
+@author: lucas
+'''
+strn = ("A meningite eh um processo inflamatorio das meninges, membranas que envolvem o cerebro e" +
+    " a medula espinhal. Pode ser causada por diversos agentes infecciosos, como bactehrias, vihrus," +
+    " parasitas e fungos, ou tambem por processos nao infecciosos. As meningites bacterianas e virais" +
+    " sao as mais importantes do ponto de vista da sauhde puhblica, devido sua magnitude, capacidade de" +
+    " ocasionar surtos e, no caso da meningite bacteriana, a gravidade dos casos. No Brasil, a meningite" +
+     " eh considerada uma doensa endemica, deste modo, casos da doensa sao esperados ao longo de todo o" +
+     " ano, com a ocorrencia de surtos e epidemias ocasionais. A ocorrencia das meningites bacterianas" +
+     " eh mais comum no inverno e, das virais, no verao.")
+    
--- Ehealth/Test/Test.py
+++ Ehealth/Test/Test.py
@@ -1,3 +1,4 @@
+# encoding: utf-8
 '''
 Created on 25 de abr de 2018
 
@@ -8,19 +9,87 @@ from gtts import gTTS
 import os
 import pyttsx
 
+
 r = sr.Recognizer()
 m = sr.Microphone()
 
 
 
 
+def msg_nao_entendi():
+    os.system("mpg321 nao_entendi.mp3")
+    
+
+def msg_conf():
+    os.system("mpg321 silencio.mp3")
+
+def conf():
+    os.system("mpg321 conf.mp3")
+
+def get_intro():
+    os.system("mpg321 intro.mp3")
+
+
+def get_info_meningite():
+    os.system("mpg321 meningite.mp3")
+
+def yes_or_no():
+    os.system("mpg321 yes_or_no.mp3")
+
+def get_pergunta_yes_or_no():
+    while True:
+        try:
+            print "Fale pausadamente"
+            yes_or_no()
+            audio = r.listen(source,timeout=2)
+            try:
+                print "Traduzindo para texto..."
+                value = r.recognize_google(audio)
+                print "Voce disse "+value
+                if value == 'yes':
+                    return True
+                elif value == 'no': 
+                    return False
+                else: msg_nao_entendi()
+            except sr.UnknownValueError:
+                print("Desculpe, nao entendi")
+                msg_nao_entendi()
+            except sr.RequestError:
+                print("Desculpe, nao entendi")
+                msg_nao_entendi()
+        except KeyboardInterrupt:
+            pass
+    
+
+def get_voice():
+    try:
+        with m as source: audio = r.listen(source,timeout=3)
+        try:
+            print "Fale pausadamente"
+            value = r.recognize_google(audio)
+            print "Voce disse "+value
+            return value
+        except sr.UnknownValueError:
+            print("Nao entendi")
+            msg_nao_entendi()
+        except sr.RequestError:
+            msg_nao_entendi()
+            print("Nao entendi")
+    except KeyboardInterrupt:
+        msg_nao_entendi()    
 
 
 
-def tell_this_in(tell,lang):
+def tell_this(tell,lang="pt"):
     tts = gTTS(text=tell, lang=lang)
-    tts.save("1.mp3")
-    os.system("mpg321 /home/lucas/workspace/E-health/Test/1.mp3")
+    tts.save("yes_or_no.mp3")
+    os.system("mpg321 yes_or_no.mp3")
+
+
+def tell_this_file(tell,file_use,lang="pt"):
+    tts = gTTS(text=tell, lang=lang)
+    tts.save(file_use+".mp3")
+    os.system("mpg321 "+file_use+".mp3")
 
 
 def tell_this_only_english(word_to_tell):
@@ -29,10 +98,74 @@ def tell_this_only_english(word_to_tell):
     engine.runAndWait()
 
 
+
+msg_conf()
+with m as source: r.adjust_for_ambient_noise(source)
+conf()
+get_intro()
+with m as source: 
+    while True:
+        try:
+            print "Fale pausadamente"
+            yes_or_no()
+            audio = r.listen(source,timeout=2)
+            try:
+                print "Traduzindo para texto..."
+                value = r.recognize_google(audio)
+                print "Voce disse "+value
+                if value == 'yes':
+                    get_info_meningite()
+                    break
+                elif value == 'no': break
+                else: msg_nao_entendi()
+            except sr.UnknownValueError:
+                print("Desculpe, nao entendi")
+                msg_nao_entendi()
+            except sr.RequestError as e:
+                print("Desculpe, nao entendi")
+                msg_nao_entendi()
+        except KeyboardInterrupt:
+            pass
+
+    s = ("Vou fazer algumas perguntas, sobre sua condisao geral. Baseado num artigo"+
+        " do hospotal Albert Einsten")
+    tell_this_file(s, "pergunta_0")
+    s = "Voce sente dores locais nas costas, nos musculos ou pescoso?"
+    tell_this_file(s, "pergunta_1")
+    #r_1 = get_pergunta_yes_or_no()
+    s = ("Voce ocasionalmente sente calafrios, fadiga, febre, letargia, mal-estar,"+
+     "perda de apetite ou tremores no corpo?")
+    tell_this_file(s, "pergunta_2")
+    #r_2 = get_pergunta_yes_or_no()
+
+    s = ("Sobre seu aparelho gastrointestinal, voce sente nahusea ou vmito")
+    tell_this_file(s, "pergunta_3")
+    #r_3 = get_pergunta_yes_or_no()
+    
+    
+    s = ("Com relasao a sua pele... Ela apresenta erupses, um pouco avermelhadas ou"+ 
+    " manchas vermelhas espalh")
+    tell_this_file(s, "pergunta_4")
+    #r_4 = get_pergunta_yes_or_no()
+    #print r_1    
+    
+
+
+
+
+
+
+'''
+    
 try:
     
-    #strn = "Silencio por favor. Estou Configurando threshold. Tudo pronto, o que voce deseja mestre?"
-    print("Silencio por favor ...")
+    #strn = "Inflamação das membranas que revestem o cérebro e a medula espinhal, geralmente causada por uma infecção."
+    
+    
+    
+    
+    #tell_this(strn)
+    print("")
     #tell_this_in(strn, "pt")
     with m as source: r.adjust_for_ambient_noise(source)
     #print("Configurando threshold para {}".format(r.energy_threshold))
@@ -43,12 +176,13 @@ try:
     try:
         # recognize speech using Google Speech Recognition
         value = r.recognize_google(audio)
-        value = "I got it, you said "+value 
+        value = "Entendi, voce disse "+value 
         print (value)
-        tell_this_only_english(value)
+      #  tell_this(value)
     except sr.UnknownValueError:
         print("Oops! Didn't catch that")
     except sr.RequestError as e:
         print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
 except KeyboardInterrupt:
     pass
+'''
